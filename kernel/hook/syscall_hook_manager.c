22e06802301b1906c596543f9c95caf6d5e1809b
SupeChicken666 <me@supechicken666.dev>
2026-08-28 17:38:35 +0800

    kernel: Relax seccomp rule for reboot() syscall on init
    Signed-off-by: SupeChicken666 <me@supechicken666.dev>

--- kernel/hook/syscall_hook_manager.c
+++ kernel/hook/syscall_hook_manager.c
@@ -20,6 +20,7 @@
 #include "hook/setuid_hook.h"
 #include "hook/syscall_hook.h"
 #include "hook/syscall_event_bridge.h"
+#include "infra/seccomp_cache.h"
 #if defined(__riscv)
 #include "hook/riscv64/syscall_regs.h"
 #endif
@@ -138,7 +139,8 @@ void ksu_process_exec_handler(void *data, struct task_struct *p, pid_t old_pid, 
 {
     // Set tracepoint flag for init process
     if (task_pid_vnr(p) == 1 && strcmp(bprm->filename, "/system/bin/init") == 0) {
-        pr_info("hook_manager: Android init namespace started\n");
+        pr_info("hook_manager: Android init namespace started (PID %i)\n", p->pid);
+        ksu_seccomp_allow_cache(current->seccomp.filter, __NR_reboot);
         ksu_set_task_tracepoint_flag(p);
     }
 }
